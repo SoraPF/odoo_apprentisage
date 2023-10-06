be4bd0085f279a6bbd903b45ee9a69e25d75ff16
SoraPF <jason.jissang@gmail.com>
2023-10-06 11:36:30 -1000

    add css and js file

--- frontend_contact/static/javascript.js
+++ frontend_contact/static/javascript.js
@@ -1,7 +1,17 @@
-myFunction(){
-// viens récupérer les id des balise pour leur information ou les modifier
-    var input = document.getElementById("search_bar");
-    var table = document.getElementById("bonjour");
-
-    table.textContent = input.value;
-}
+var input = document.getElementById("search_bar");
+var table = document.getElementById("table");
+var rows = Array.from(table.rows);
+input.addEventListener("input", function() {
+    var searchValue = input.value.toLowerCase(); // Convertir en minuscules pour une comparaison insensible à la casse
+    rows.forEach(function(row) {
+        var cells = Array.from(row.cells);
+        cells.forEach(function(cell) {
+            var cellText = cell.textContent.toLowerCase(); // Convertir en minuscules pour une comparaison insensible à la casse
+            if (cellText.includes(searchValue)) {  //si la valeur appartien a la cellules
+                cell.style.backgroundColor = "yellow"; // Mettre en évidence les cellules correspondantes
+            } else {
+                cell.style.backgroundColor = ""; // Réinitialiser la couleur de fond des autres cellules
+            }
+        });
+    });
+});
