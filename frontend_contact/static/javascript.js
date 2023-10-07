10f2aeb963cddf1518000db5584776555f2991f1
SoraPF <jason.jissang@gmail.com>
2023-10-06 16:21:04 -1000

    start ajax, add css, add test balise for ajax

--- frontend_contact/static/javascript.js
+++ frontend_contact/static/javascript.js
@@ -1,6 +1,9 @@
 var input = document.getElementById("search_bar");
 var table = document.getElementById("table");
 var rows = Array.from(table.rows);
+var testing = document.getElementById("test")
+/*
+//highlight
 input.addEventListener("input", function() {
     var searchValue = input.value.toLowerCase(); // Convertir en minuscules pour une comparaison insensible à la casse
     rows.forEach(function(row) {
@@ -8,10 +11,23 @@ input.addEventListener("input", function() {
         cells.forEach(function(cell) {
             var cellText = cell.textContent.toLowerCase(); // Convertir en minuscules pour une comparaison insensible à la casse
             if (cellText.includes(searchValue)) {  //si la valeur appartien a la cellules
-                cell.style.backgroundColor = "yellow"; // Mettre en évidence les cellules correspondantes
+                cell.style.backgroundColor = "#6d071a"; // Mettre en évidence les cellules correspondantes
             } else {
                 cell.style.backgroundColor = ""; // Réinitialiser la couleur de fond des autres cellules
             }
         });
     });
 });
+*/
+//ajax
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "/frontend_contact/contact", true);
+xhr.send();
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var responseData = JSON.parse(xhr.responseText);
+
+    }
+};
