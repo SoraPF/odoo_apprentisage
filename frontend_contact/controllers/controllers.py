347760e303bf70b3c5f0286fb49900f02dd01cb9
SoraPF <jason.jissang@gmail.com>
2023-10-19 16:15:54 -1000

    clean commentary controller

--- frontend_contact/controllers/controllers.py
+++ frontend_contact/controllers/controllers.py
@@ -9,50 +9,50 @@ class FrontendContact(http.Controller):
     @http.route('/frontend_contact/contact', website=True, auth='user')
     def index(self,**kw):
         _logger = logging.getLogger("frontend_contact.frontend_contact")
-        #var pour stoquer plus tard les donnée
+
         contact_per_page = 10
+
         direction = request.params.get('direction')
+
         term = request.params.get('term')
+
         nextPage = request.params.get('newPage')
 
-        if 'current_page' not in request.session: #cree une valeur dans la session
+        if 'current_page' not in request.session:
             request.session['current_page'] = 1
 
-        _logger.info("page>>>>>>>>>>>>>>>>>>>>>>>>>> %s", request.session['current_page'])
-        _logger.info("nextPage>>>>>>>>>>>>>>>>>>>>>>>>>> %s", nextPage)
+        current_page = request.session['current_page']
 
-        # si y a un requête avec une method = POST
         if request.httprequest.method == 'POST':
-            term = request.params['searchBar']  # la var data vas stoquer ce que contenais la balise id=searchBar
-            _logger.info("term>>>>>>>>>>>>>>>>>>>>>>>>>> %s", term)
-            # creer un session pour gardé en mémoire le term
+            term = request.params['searchBar']
+
             if 'searchBarInput' not in request.session:
                 request.session['searchBarInput'] = term
             if term != request.session['searchBarInput']:
                 request.session['searchBarInput'] = term
-                request.session['current_page'] = 1
+                current_page = 1
 
         tableau = (request.env['res.partner'].sudo().search([('name', 'ilike', term)]) or request.env['res.partner'].sudo().search([('mobile', 'ilike', term)]))
+
         limiteOffset = theLimiteOffset(tableau, contact_per_page)
 
-        _logger.info("limitpage>>>>>>>>>>>>>>>>>>>>>>>>>> %s", limiteOffset)
+        if(direction == '1') and current_page < limiteOffset:
+            current_page += 1
+        elif(direction) == '-1' and current_page > 1:
+            current_page -= 1
 
-        if(direction == '1') and request.session['current_page'] < limiteOffset:
-            request.session['current_page'] += 1
-        elif(direction) == '-1' and request.session['current_page'] > 1:
-            request.session['current_page'] -= 1
-        if nextPage != None:
-            request.session['current_page'] = int(nextPage)
-        offset = (request.session['current_page'] - 1) * contact_per_page
-                #les contacts qui seront afficher par 10 avec pour contrainte nom ou mobile
+        if nextPage != None and isinstance(variable, int):
+            current_page = int(nextPage)
+
+        offset = (current_page - 1) * contact_per_page
+
         contact = (request.env['res.partner'].sudo().search([('name', 'ilike', term)], limit=contact_per_page, offset=offset) or
                    request.env['res.partner'].sudo().search([('mobile', 'ilike', term)], limit=contact_per_page,offset=offset))
 
-        _logger.info("contact>>>>>>>>>>>>>>>>>>>>>>>>>> %s", contact)
+        pages = LimitButtonPages(limiteOffset+1,current_page)
 
-        pages = LimitButtonPages(limiteOffset+1,request.session['current_page'])
+        request.session['current_page'] = current_page
 
-        #retourner la page html avec les donné venant des variable contact et data
         return request.render("frontend_contact.list_contact_page", {'contact': contact,
                                                                      'pages': pages,
                                                                      'input_data': term,
