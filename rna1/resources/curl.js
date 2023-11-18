ef78c5da1535090db334346c3fda0cb7298a3a65
SoraPF <jason.jissang@gmail.com>
2023-11-17 15:40:59 -1000

    curl authentification response ok

--- rna1/resources/curl.js
+++ rna1/resources/curl.js
@@ -1,23 +1,29 @@
 const requestCurl = async (username, password) => {
     let data = JSON.stringify({
+      jsonrpc: '2.0',
+      method: 'call',
+      params: {
         db: 'odoo',
         login: username,
         password: password,
+      },
     });
 
-    fetch('http://127.0.0.1:8069/web/session/authenticate',{
-        jsonrpc: '2.0',
-        method:'POST',
-        headers:{
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-        data:data,
-    }).then(response =>{
-        console.log("response",response);
-    }).catch(error =>{
-        console.log("error:",error);
+    fetch('http://127.0.0.1:8069/web/session/authenticate', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: data,
     })
+      .then(response => response.json())
+      .then(data => {
+        console.log('response', data.result);
+      })
+      .catch(error => {
+        console.log('error:', error);
+      });
 
 }
 
